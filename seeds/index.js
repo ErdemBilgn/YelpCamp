d5beab20633a37f7077d6c3b739902c612535cdb
Erdem <mr.erdembilgin@gmail.com>
2023-11-29 17:47:07 +0300

    Commit: Added basic styles

--- seeds/index.js
+++ seeds/index.js
@@ -15,13 +15,18 @@ db.once("open", () => {
 const sample = (arr) => arr[Math.floor(Math.random() * arr.length)]; // Just returns a random element of the passed array.
 
 
+
 const seedDB = async() => {
   await Campground.deleteMany({});
   for (let i = 0; i < 50; i++){
     const random1000 = Math.floor(Math.random() * 1000);
+    const price = Math.floor(Math.random() * 30) + 10;
     const camp = new Campground({
       location: `${cities[random1000].city}, ${cities[random1000].state}`,
-      title: `${sample(descriptors)} ${sample(places)}`
+      title: `${sample(descriptors)} ${sample(places)}`,
+      image: "https://source.unsplash.com/collection/483251",
+      description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellendus optio, voluptatem veritatis ea, nesciunt corrupti, distinctio architecto quae dolores minima qui beatae. Magni animi dolore nisi earum, accusantium magnam vero.",
+      price
     })
     await camp.save();
   }
